64196d1078a4af2804051f761f3dfc25753aeaf7
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-11-12 18:31:44 +0530

    Create Application class

--- app/src/main/java/com/example/jetpack_food_recipes/domain/model/Recipe.kt
+++ app/src/main/java/com/example/jetpack_food_recipes/domain/model/Recipe.kt
@@ -1,3 +1,14 @@
 package com.example.jetpack_food_recipes.domain.model
 
-data class Recipe()
+
+data class Recipe(
+    val id: Int,
+    val title: String,
+    val publisher: String,
+    val featuredImage: String,
+    val rating: Int = 0,
+    val sourceUrl: String,
+    val ingredients: List<String> = listOf(),
+    val dateAdded: String,
+    val dateUpdated: String,
+)
