c9fb13fd2711cd52b1e4dd453da88512f3e4e64f
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-11-13 19:19:23 +0530

    Create EntityMapper interface to handle mapping between data entity and domain model

---
+++ app/src/main/java/com/example/jetpack_food_recipes/domain/util/EntityMapper.kt
@@ -0,0 +1,8 @@
+package com.example.jetpack_food_recipes.domain.util
+
+interface EntityMapper<Entity, DomainModel> {
+
+    fun mapFromEntity(entity: Entity): DomainModel
+
+    fun mapToEntity(domainModel: DomainModel): Entity
+}
