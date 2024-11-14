def3ee54a8b29521b03056b9a67e1bb26536b6b4
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-11-14 20:25:11 +0530

    Setup Recipe Repository

---
+++ app/src/main/java/com/example/jetpack_food_recipes/repository/RecipeRepository.kt
@@ -0,0 +1,10 @@
+package com.example.jetpack_food_recipes.repository
+
+import com.example.jetpack_food_recipes.domain.model.Recipe
+
+interface RecipeRepository {
+
+    suspend fun search(token: String, page: Int, query: String): List<Recipe>
+
+    suspend fun get(token: String, id: Int): List<Recipe>
+}
