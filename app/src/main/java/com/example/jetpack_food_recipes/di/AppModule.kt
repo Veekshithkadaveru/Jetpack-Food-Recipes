28626b4cb9c8125921bce534c5791353aa17d8f2
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-11-18 19:27:17 +0530

    Implement Hilt module for dependency injection of BaseApplication

---
+++ app/src/main/java/com/example/jetpack_food_recipes/di/AppModule.kt
@@ -0,0 +1,23 @@
+package com.example.jetpack_food_recipes.di
+
+import android.content.Context
+import com.example.jetpack_food_recipes.BaseApp
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext app: Context): BaseApp {
+        return app as BaseApp
+    }
+
+}
